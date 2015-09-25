2be9c04ef3eefcd369bd9b159aaef0cb6dd12339
Elisa Cutrin <ecutrin@thoughtworks.com>
2015-09-25 18:58:45 +0200

    Removing unused code

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -6,18 +6,6 @@ class Serie < ActiveRecord::Base
   BASE_URL = "https://image.tmdb.org/t/p/w185/"
   NOT_FOUND_IMAGE = "/assets/image_not_found.jpg" 
 
- # def initialize(attributes)
-    #@title = attributes[:title]
-    #@picture_url = attributes[:picture_url].nil? ? NOT_FOUND_IMAGE : BASE_URL + attributes[:picture_url]
-    #@show_id = attributes[:show_id]
-  #end
-
- # def initialize(title, picture_url, show_id)
-    #@title = title
-    #@picture_url = picture_url.nil? ? NOT_FOUND_IMAGE : BASE_URL + picture_url
-    #@show_id = show_id
-  #end
-
   def picture_url
     self[:picture_url].nil? ? NOT_FOUND_IMAGE : BASE_URL + self[:picture_url]
   end
